f5ddf0c0379498f4d4e64f9a1227c94d7a13c472
boringtao <boringtaoxh@gmail.com>
2015-10-31 21:47:16 +0800

    password check initial state

--- app/app/directives/passwordCheck.js
+++ app/app/directives/passwordCheck.js
@@ -3,8 +3,7 @@ angular
     return {
       require: 'ngModel',
       link: function (scope, elem, attrs, ctrl) {
-        var v = elem.val() === $('#userPassword').val();
-        scope.passwordMatch = v;
+        scope.passwordMatch = true;
         $('#confirmPassword').on('keyup', function () {
           scope.$apply(function () {
             var v = elem.val() === $('#userPassword').val();
